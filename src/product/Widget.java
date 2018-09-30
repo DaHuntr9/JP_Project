310934709d895a43aec3c8b89aea7d17dd7c1d84
Hunter <hunterdstudent@gmail.com>
2018-09-30 00:00:02 -0400

    Second Push Bug Fixes and Styling edits

--- src/product/Widget.java
+++ src/product/Widget.java
@@ -1,5 +1,12 @@
 package product;
 
+/**
+ *Programmer Name: Hunter Danielson
+ *Date Created:9/29/2018
+ *Description of file:
+ *This file was created because of check style
+ */
+
 //class for widget that will utilize products classes
 class Widget extends Product {
 
